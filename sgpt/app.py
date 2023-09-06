7f45a217b881940721b9bf8c8c0136befff21fc2
Nwdwn <10559837+Nwdwn@users.noreply.github.com>
2023-09-06 18:45:07 +0200

    Add files via upload

--- sgpt/app.py
+++ sgpt/app.py
@@ -1,7 +1,5 @@
-# To allow users to use arrow keys in the REPL.
 import readline  # noqa: F401
 import sys
-
 import typer
 from click import BadArgumentUsage, MissingParameter
 from click.types import Choice
@@ -13,6 +11,35 @@ from sgpt.handlers.repl_handler import ReplHandler
 from sgpt.role import DefaultRoles, SystemRole
 from sgpt.utils import get_edited_prompt, install_shell_integration, run_command
 
+def handle_second_ai(chat_handler, second_ai_prompt, model, temperature, top_probability, chat_id, caching):
+    chat_handler.handle(
+        second_ai_prompt,
+        model=model,
+        temperature=temperature,
+        top_probability=top_probability,
+        chat_id=chat_id,
+        caching=caching,
+    )
+
+def handle_user_input(
+    chat_handler: ChatHandler,
+    user_input: str,
+    model: str,
+    temperature: float,
+    top_probability: float,
+    chat_id: str,
+    caching: bool,
+):
+    user_response = chat_handler.handle(
+        user_input,
+        model=model,
+        temperature=temperature,
+        top_probability=top_probability,
+        chat_id=chat_id,
+        caching=caching,
+    )
+    print("You:", user_input)
+    print("AI:", user_response)
 
 def main(
     prompt: str = typer.Argument(
@@ -65,7 +92,7 @@ def main(
     ),
     chat: str = typer.Option(
         None,
-        help="Follow conversation with id, " 'use "temp" for quick session.',
+        help="Follow conversation with id, use 'temp' for quick session.",
         rich_help_panel="Chat Options",
     ),
     repl: str = typer.Option(
@@ -114,11 +141,39 @@ def main(
         callback=install_shell_integration,
         hidden=True,  # Hiding since should be used only once.
     ),
+    second_ai: bool = typer.Option(
+        False,
+        help="Enable the second AI instance to receive prompts from the initial AI prompt.",
+    ),
+    second_ai_prompt: str = typer.Option(
+        None,
+        help="Second AI prompt (use with --second-ai to pass a prompt to the second AI instance).",
+    ),
+    interactive_mode: bool = typer.Option(
+        False,
+        "--interactive",
+        help="Enter interactive mode to provide user input during conversation.",
+    ),
 ) -> None:
     stdin_passed = not sys.stdin.isatty()
 
     if stdin_passed and not repl:
         prompt = f"{sys.stdin.read()}\n\n{prompt or ''}"
+
+    if interactive_mode:
+        chat_handler = ChatHandler(chat, SystemRole.get(role))
+
+        while True:
+            user_input = input("You: ")
+            handle_user_input(
+                chat_handler,
+                user_input,
+                model=model,
+                temperature=temperature,
+                top_probability=top_probability,
+                chat_id=chat,
+                caching=cache,
+            )
 
     if not prompt and not editor and not repl:
         raise MissingParameter(param_hint="PROMPT", param_type="string")
@@ -134,9 +189,6 @@ def main(
     if editor and stdin_passed:
         raise BadArgumentUsage("--editor option cannot be used with stdin input.")
 
-    if editor:
-        prompt = get_edited_prompt()
-
     role_class = (
         DefaultRoles.check_get(shell, describe_shell, code)
         if not role
@@ -144,7 +196,7 @@ def main(
     )
 
     if repl:
-        # Will be in infinite loop here until user exits with Ctrl+C.
+        # Will be in an infinite loop here until the user exits with Ctrl+C.
         ReplHandler(repl, role_class).handle(
             prompt,
             model=model,
@@ -155,14 +207,45 @@ def main(
         )
 
     if chat:
-        full_completion = ChatHandler(chat, role_class).handle(
-            prompt,
-            model=model,
-            temperature=temperature,
-            top_probability=top_probability,
-            chat_id=chat,
-            caching=cache,
-        )
+        if second_ai and second_ai_prompt is not None:
+            chat_handler = ChatHandler(chat, role_class)
+            handle_second_ai(
+                chat_handler,
+                second_ai_prompt,
+                model,
+                temperature,
+                top_probability,
+                chat,
+                cache,
+            )
+        else:
+            chat_handler = ChatHandler(chat, role_class)
+            second_ai_handler = ChatHandler("second_ai", role_class)
+            while True:
+                # First AI instance
+                first_ai_response = chat_handler.handle(
+                    prompt,
+                    model=model,
+                    temperature=temperature,
+                    top_probability=top_probability,
+                    chat_id=chat,
+                    caching=cache,
+                )
+                print("First AI:", first_ai_response)
+
+                # Second AI instance
+                second_ai_response = second_ai_handler.handle(
+                    second_ai_prompt,
+                    model=model,
+                    temperature=temperature,
+                    top_probability=top_probability,
+                    chat_id="second_ai",
+                    caching=cache,
+                )
+                print("Second AI:", second_ai_response)
+
+                prompt = first_ai_response
+                second_ai_prompt = second_ai_response
     else:
         full_completion = DefaultHandler(role_class).handle(
             prompt,
@@ -181,7 +264,6 @@ def main(
             show_default=False,
         )
         if option in ("e", "y"):
-            # "y" option is for keeping compatibility with old version.
             run_command(full_completion)
         elif option == "d":
             DefaultHandler(DefaultRoles.DESCRIBE_SHELL.get_role()).handle(
@@ -192,13 +274,6 @@ def main(
                 caching=cache,
             )
             continue
-        break
-
-
-def entry_point() -> None:
-    # Python package entry point defined in setup.py
-    typer.run(main)
-
 
 if __name__ == "__main__":
-    entry_point()
+    typer.run(main)
